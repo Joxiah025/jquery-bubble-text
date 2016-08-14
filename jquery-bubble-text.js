61b403d14f90e7537874ed62623bde1be5797f3e
Washington Luís Guedes <wlg.96@hotmail.com>
2016-08-13 22:59:19 -0300

    Solved Bug #1

--- jquery-bubble-text.js
+++ jquery-bubble-text.js
@@ -1,158 +1,168 @@
-/*bubbleText({
-    element: $element,        -- mandatory, must be one DOM leaf node
-    newText: 'new Text',      -- mandatory, must be one string
-    speed: 5000,              -- optional, default: 2000
-    callback: function(){}    -- optional
-});*/
+// bubbleText({
+//     element: $element,        -- mandatory, must be one DOM leaf node
+//     newText: 'new Text',      -- mandatory, must be one string
+//     speed: 5000,              -- optional, default: 2000
+//     callback: function(){}    -- optional
+// })
 
-var bubbleText = function(o) {
-    var $$, __, bubble;
+function bubbleText(o) {
 
-    if (!o | typeof o != 'object' | o.map == [].map) {
-        throw 'bubbleText() expects an object as argument';
+    // check if is an object
+    if (o !== Object(o) || o.map === [].map) {
+        throw 'bubbleText: Missing properties';
         return;
     }
 
-    if (!(__ = ($$ = $(o.element))[0]) || $$.find(':first').length) {
-        throw 'bubbleText() expects one DOM leaf in the property "element".';
+    // get element
+    var $element = $(o.element);
+    var dom = $element[0];
+
+    // check if has element
+    if (!dom) {
+        throw 'bubbleText: Missing element';
         return;
     }
 
-    if (typeof o.newText != 'string') {
-        throw 'bubbleText() expects one string in the property "newText".';
+    // check if is valid element
+    var hasChild = $element.find(':first').length;
+    if (hasChild) {
+        throw 'bubbleText: Element must be one leaf node';
         return;
     }
 
-    // space's width
-    var span = $('<span data-bubbletext>w w</span>').prependTo($$);
-    var spaceWidth = span.width();
-    span.html('w');
-    spaceWidth -= 2 * span.width();
+    // check if has newText
+    var newText = o.newText;
+    if (typeof newText !== 'string') {
+        throw 'bubbleText: Missing newText';
+        return;
+    }
 
-    // height for all spans
-    var height = span.css('height');
-    span.remove();
+    // empty $element
+    var oldText = dom.innerHTML;
+    dom.innerHTML = '';
 
-    // store properties from each old letter
-    var oldTable = [];
-    __.innerHTML = __.innerHTML.replace(/./g, function(x, i) {
-        oldTable.push({
-            char: x,
-            index: i
-        });
-        var style = x == ' ' ? ' style="width:' + spaceWidth + 'px"' : '';
-        return '<span data-bubbletext="old' + i + '"' + style + '>' + x + '</span>';
-    });
+    // map spans
+    function mapString(string) {
+        var array = [];
+        for (var i = 0, l = string.length; i < l; i++) {
+            var char = string.charAt(i);
+            char = char === ' ' ? '&nbsp;' : char;
+            array[i] = {
+                char: char,
+                html: $('<span class="span-bubble-text">' + char + '</span>')
+                    .appendTo($element),
+            };
+        }
+        return array;
+    };
+    var newSpans = mapString(newText);
+    var oldSpans = mapString(oldText);
 
-    // store properties from each new letter
-    var newTable = [];
-    __.innerHTML = o.newText.replace(/./g, function(x, i) {
-
-        // index of this letter in the old text
-        var oldIndex = -1;
-        for (var i = 0, l = oldTable.length; i < l; i++) {
-            if (oldTable[i].char == x) {
-                oldIndex = oldTable[i].index;
-                oldTable.splice(i, 1);
+    // preload the order of all animations
+    var animations = [];
+    while (newSpans.length) {
+        var add = newSpans.shift();
+        var step = {
+            add: add,
+            width: add.html.width() + 'px', // test
+        };
+        add.html.width(0);
+        var char = add.char;
+        for (var i = 0, l = oldSpans.length; i < l; i++) {
+            if (char === oldSpans[i].char) {
+                step.remove = oldSpans.splice(i, 1)[0];
                 break;
             }
+        };
+        animations.push(step);
+    };
+    var indexRemotion = 0;
+    while (oldSpans.length) {
+        var step = {
+            remove: oldSpans.shift(),
+        };
+        if (indexRemotion < animations.length) {
+            animations.splice(indexRemotion, 0, step);
+            indexRemotion += 2;
+        } else {
+            animations.push(step);
         }
-
-        // fill this letter's data
-        newTable.push({
-            char: x,
-            index: i,
-            old: oldIndex
-        });
-
-        return '<span data-bubbletext=new>' + x + '</span>';
-    }) + __.innerHTML;
+    };
 
     // force spans to be aligned as normal text
-    var spans = $$.find('span').each(function() {
-        $(this).css({ position: 'relative', display: 'inline-block', height: height, overflow: 'hidden' });
-
-        // store new letters width and prepare them to animate
-        if ($(this).data('bubbletext') == 'new') {
-            $(this).attr('data-bubbletext', $(this).width() || spaceWidth).width(0);
-        }
+    var spans = $element.find('span');
+    var spanHeight = $(spans[0]).height(); // test
+    spans.each(function() {
+        $(this).css({
+            position: 'relative',
+            display: 'inline-block',
+            height: spanHeight + 'px',
+            overflow: 'hidden',
+        });
     });
 
-    // mark old spans to be removed
-    for (var i = 0, l = oldTable.length; i < l; i++) {
-        $$.find('[data-bubbletext=old' + oldTable[i].index + ']:first').attr('data-bubbletext-action', 'remove');
-    }
-
-    // speed for each animation's step
-    var speed = (o.speed || 2000) / (oldTable.length + newTable.length);
-
-    // Rule when it should add (false||0) or remove (true||1) letters
-    var timeToRemove = 1,
-        additionsDone = 0,
-        remotionsDone = 0;
-
-    var spansToRemove = $$.find('[data-bubbletext-action=remove]'),
-        remotionIndex = 0;
+    // animation global properties
+    var speed = Math.floor((o.speed || 2000) / animations.length);
+    var boundaries = ['&nbsp;', '.', ','];
 
     // start animations
-    (bubble = function(spanIndex) {
-        if (additionsDone && remotionsDone) {
-            __.innerHTML = o.newText;
-            o.callback instanceof Function && o.callback();
+    (function bubble(position) {
+        var step = animations[position];
+
+        // if animations ended
+        if (!step) {
+            dom.innerHTML = newText;
+            if (typeof o.callback === 'function') {
+                o.callback();
+            }
             return;
         }
 
-        // function to trigger next animation
-        var nextAnimation = function() {
-            add = timeToRemove + remotionsDone;
-            if (remotionsDone == 2) {
-                remotionsDone = 1;
-                add -= 2;
-            }
-            bubble(spanIndex + add);
+        // animation properties
+        var nextAnimation = function() { bubble(position + 1); };
+        var objAnimate = {
+            duration: speed,
+            complete: nextAnimation,
+            easing: 'linear',
         };
 
-        // if it is time to remove
-        if (timeToRemove) {
-            if (!additionsDone) {
-                timeToRemove = 0;
-            }
+        // animate span letter in
+        if (step.add) {
+            objAnimate.complete = function() {
+                var prev = step.add.html.prev('span');
+                if (prev.length) {
+                    var previousTop = prev.offset().top;
+                    var thisTop = step.add.html.offset().top;
 
-            if (remotionIndex == spansToRemove.length) {
-                remotionsDone = 2;
-                nextAnimation();
-                return;
-            }
+                    // if need to break line
+                    if (thisTop !== previousTop) {
 
-            var span = $(spansToRemove[remotionIndex++]);
-            span.animate({ width: 0 }, speed, nextAnimation);
-        }
-
-        // else, time to show letter
-        else {
-            if (!remotionsDone) {
-                timeToRemove = 1;
-            }
-
-            var element = spans.eq(spanIndex);
-            var width = +element.attr('data-bubbletext');
-
-            if (width) {
-                var properties = newTable[spanIndex];
-
-                // if there is one correspondent span from oldText
-                if (~properties.old) {
-                    var span = $$.find('[data-bubbletext=old' + properties.old + ']:first');
-                    span.animate({ width: 0 }, speed);
+                        // find previous boundary span to display block
+                        while (boundaries.indexOf(prev.html()) === -1) {
+                            prev = prev.prev('span');
+                            if (prev.length === 0) {
+                                break;
+                            }
+                        }
+                        prev.after('<br>');
+                    }
                 }
-                element.animate({ width: width }, speed, nextAnimation);
-
-            } else {
-                additionsDone = 1;
                 nextAnimation();
-            }
+            };
+            step.add.html.animate({
+                width: step.width,
+            }, objAnimate);
         }
 
+        // animate span letter out
+        if (step.remove) {
+            step.remove.html.animate({
+                width: 0,
+            }, step.add ? {
+                duration: speed,
+                easing: 'linear',
+            } : objAnimate);
+        }
     })(0);
 
 };
